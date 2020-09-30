135471859310c1ee2c89576577e1a36e3cd5237e
khaledzaki2017 <khaledzaky07@gmail.com>
2020-09-30 21:37:28 +0200

    d

--- src/screens/confirmation/validateNumber.js
+++ src/screens/confirmation/validateNumber.js
@@ -3,90 +3,90 @@ import { connect } from 'react-redux'
 import ReactCodeInput from 'react-verification-code-input';
 import KeyboardBackspaceIcon from '@material-ui/icons/KeyboardBackspace';
 import Button from '@material-ui/core/Button';
-import {API_URL} from '../../config';
+import { API_URL } from '../../config';
 import axios from 'axios';
 import { useSelector } from 'react-redux';
 
- const ValidateNumber = ({history}) => {
+const ValidateNumber = ({ history }) => {
 
-    
-    const [number,setNumber]=React.useState('');
-    const [serverNumber,setServerNumber]=React.useState(null);
-    const general_info = useSelector(state=> state.wizard.step1_natural)
+
+    const [number, setNumber] = React.useState('');
+    const [serverNumber, setServerNumber] = React.useState(null);
+    const general_info = useSelector(state => state.wizard.step1_natural)
 
 
     useEffect(() => {
 
-        const fetch= async ()=>{
-    
-            axios.post(`${API_URL}api/verify`,{phone:general_info.mobile_phone})
-            .then((res)=>{
-              console.log(res)
-            })
-            .catch((err)=>console.log(err))
-    
-     }
-     
-     
-     fetch();
-         
-       }, []);  
+        const fetch = async () => {
 
-    const handelComplete = (event) =>{
+            axios.get('http://localhost:8000/api/verify/01011254202/')
+                .then((res) => {
+                    console.log(res)
+                })
+                .catch((err) => console.log(err))
+
+        }
+
+
+        fetch();
+
+    }, []);
+
+    const handelComplete = (event) => {
         console.log(event)
         setNumber(event)
 
     }
-    const handelClick =()=>{
+    const handelClick = () => {
         //check number
         history.push('/final')
 
 
     }
     return (
-        <div style={{marginTop:'5px'}}>
-        <div className='titleAction'> 
-          <p className= 'textAction'>FIRMA DEL CONTRATO</p> 
-        </div>
-        <div className='confirmContent'>
-            <div className='back'>
-                <KeyboardBackspaceIcon/>
+        <div style={{ marginTop: '5px' }}>
+            <div className='titleAction'>
+                <p className='textAction'>FIRMA DEL CONTRATO</p>
             </div>
+            <div className='confirmContent'>
+                <div className='back'>
+                    <KeyboardBackspaceIcon />
+                </div>
 
-        <div className='confirmContiner'>
-            <p className='confirm_bold'>
-            DATOS PERSONALES DEL FIRMANTE
+                <div className='confirmContiner'>
+                    <p className='confirm_bold'>
+                        DATOS PERSONALES DEL FIRMANTE
             </p>
-            <p className='confirm_normal'>
-            La información tiene que ser la misma del inicio del registro
+                    <p className='confirm_normal'>
+                        La información tiene que ser la misma del inicio del registro
             </p>
-        </div>
-        <div style={{flexGrow:1}}></div>
+                </div>
+                <div style={{ flexGrow: 1 }}></div>
 
-        </div>
-        <div className='vaildate'>
-            <ReactCodeInput  onComplete={(e)=>handelComplete(e)}/>
-        <Button 
-                  className='btn' 
-                  style={{marginTop:'6%',width:'15%'}}
-                  type='submit'
-                  variant="outlined" 
-                  onClick={handelClick}
-              
-                  >
-                     Continue
+            </div>
+            <div className='vaildate'>
+                <ReactCodeInput onComplete={(e) => handelComplete(e)} />
+                <Button
+                    className='btn'
+                    style={{ marginTop: '6%', width: '15%' }}
+                    type='submit'
+                    variant="outlined"
+                    onClick={handelClick}
+
+                >
+                    Continue
         </Button>
-        </div>
+            </div>
         </div>
     )
 }
 
 const mapStateToProps = (state) => ({
-    
+
 })
 
 const mapDispatchToProps = {
-    
+
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(ValidateNumber)
